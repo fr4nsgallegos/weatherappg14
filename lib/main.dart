11ef88d94f8c4cbf9febcdf5e3df50d9adc5e82b
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-01-08 20:34:58 -0500

    search city widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:weatherappg14/pages/home_page.dart';
 
 void main() {
-  runApp(MaterialApp(home: HomePage()));
+  runApp(MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false));
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:weatherappg14/widget/search_city_widget.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  TextEditingController _searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color(0xff2C2F31),
+      appBar: AppBar(
+        title: Text("Weather App", style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+        backgroundColor: Color(0xff2C2F31),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.location_on_outlined, color: Colors.white),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          SearchCityWidget(controller: _searchController, function: () {}),
+        ],
+      ),
+    );
   }
 }
