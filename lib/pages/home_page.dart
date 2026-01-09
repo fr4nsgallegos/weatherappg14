5090de4dd528c08d375000d5c0382e1a9987d93b
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-01-08 20:52:45 -0500

    container de info wehater

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weatherappg14/widget/search_city_widget.dart';
+import 'package:weatherappg14/widget/weather_item.dart';
 
 class HomePage extends StatelessWidget {
   TextEditingController _searchController = TextEditingController();
@@ -22,6 +23,41 @@ class HomePage extends StatelessWidget {
       body: Column(
         children: [
           SearchCityWidget(controller: _searchController, function: () {}),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 24, horizontal: 20),
+            padding: EdgeInsets.symmetric(vertical: 32, horizontal: 24),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25),
+              gradient: LinearGradient(
+                colors: [Color(0xff6B9AF8), Color(0xff2E5FEC)],
+                begin: Alignment.bottomRight,
+                end: Alignment.topLeft,
+              ),
+            ),
+            child: Column(
+              children: [
+                Text(
+                  "lima, Perú",
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+                SizedBox(height: 32),
+                Image.asset("assets/icons/heavycloudy.png", height: 100),
+                Text(
+                  "23.9 °",
+                  style: TextStyle(fontSize: 100, color: Colors.white),
+                ),
+                Divider(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    WeatherItem(value: 18, unit: "km/h", image: "windspeed"),
+                    WeatherItem(value: 76, unit: "%", image: "humidity"),
+                    WeatherItem(value: 58, unit: "%", image: "cloud"),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
